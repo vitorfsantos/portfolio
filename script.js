d010f3c8de97ac5186e2b4b89c9c4c3cc372c9de
vitorfsantos <91084782+vitorfsantos@users.noreply.github.com>
2022-03-28 16:13:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const btnMobile = document.getElementById('btn-mobile')
+const click_link = document.getElementsByName('a')
+const nav = document.querySelector('nav');
+const links = document.querySelectorAll('li a');
+
+
 function toggleMenu(){
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
 }
 
 btnMobile.addEventListener('click', toggleMenu)
+
+
+links.forEach(item => {
+    item.addEventListener('click', (event) => {
+    nav.classList.toggle('active');
+    })
+  })
